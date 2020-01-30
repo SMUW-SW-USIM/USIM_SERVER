69f502e77038f0e38528c3b6e67320cbcc27f1a0
DaEunShim <10ekdms01@naver.com>
2020-01-30 16:43:48 +0900

    update

--- config/dbPool.js
+++ config/dbPool.js
@@ -1,11 +1,3 @@
-// module.exports = {
-//   host: 'database-1.ceor5h1wx6xh.ap-northeast-2.rds.amazonaws.com',
-//   port: 3306,
-//   user: 'root',
-//   password: 'data1234',
-//   database: 'care-direction',
-//   connectionLimit: 15,
-// }
 
 module.exports = {
   host: 'usim-db.cdahy2yxhypa.ap-northeast-2.rds.amazonaws.com',
